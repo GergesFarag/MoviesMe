5999bd5bdebec235aaf02b332174b089fa0a4035
GergesFarag <georgefarag128@gmail.com>
2025-08-15 01:48:05 +0300

    updating

--- api/app.ts
+++ api/app.ts
@@ -2,7 +2,7 @@ import { Request, Response } from "express";
 import app from "../src/app";
 import connectDB from "../src/Config/db";
 
-// Prevent reconnecting on every request
+
 let isConnected = false;
 
 export default async function handler(req: Request, res: Response) {
@@ -11,8 +11,7 @@ export default async function handler(req: Request, res: Response) {
       await connectDB();
       isConnected = true;
     }
-    // Pass the request to Express
-    return (app as any)(req, res);
+    return app(req, res);
   } catch (err) {
     console.error('Bootstrap error:', err);
     res.status(500).json({ status: 'error', message: 'Init failed' });
