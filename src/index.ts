480ba9a23c5ef76f47a4295b000b0fc96c37705f
GergesFarag <georgefarag128@gmail.com>
2025-08-15 00:39:04 +0300

    UPDATE

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-import app from "./app";
-import connectDB from "./Config/db";
-const PORT = process.env.PORT_NUMBER || 3000;
+import connectDB from "./Config/db";  // Your DB connection logic
+import app from "./app";  // Import the Express app
+
 (async () => {
+  // Database connection
   await connectDB();
-  const server = app.listen(PORT, () => {
-    console.log(`Sever is running on : http://localhost:${PORT}/`);
-  });
-  process.on("unhandledRejection", (error) => {
-    console.error("Unhandled Rejection:", error);
-    server.close(() => {
-      process.exit(1);
-    });
-  });
+  console.log("Connected to the database");
+  // const PORT = process.env.PORT_NUMBER || 3000;
+  // const server = app.listen(PORT, () => {
+  //   console.log(`Sever is running on : http://localhost:${PORT}/`);
+  // });
+  // process.on("unhandledRejection", (error) => {
+  //   console.error("Unhandled Rejection:", error);
+  //   server.close(() => {
+  //     process.exit(1);
+  //   });
+  // });
 })();
