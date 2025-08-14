a8a7a5c3aed3aaaddbcfb7d2d39d6fe9a610d2d7
GergesFarag <georgefarag128@gmail.com>
2025-08-15 00:52:22 +0300

    updating

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,2 @@
 import app from "./app";
-import connectDB from "./Config/db";
-const PORT = process.env.PORT_NUMBER || 3000;
-(async () => {
-  await connectDB();
-  const server = app.listen(PORT, () => {
-    console.log(`Sever is running on : http://localhost:${PORT}/`);
-  });
-  process.on("unhandledRejection", (error) => {
-    console.error("Unhandled Rejection:", error);
-    server.close(() => {
-      process.exit(1);
-    });
-  });
-})();
+
