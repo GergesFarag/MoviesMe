0af711118dac147b1630c1a56faa1e04df58e37b
GergesFarag <georgefarag128@gmail.com>
2025-08-15 00:11:43 +0300

    second one

--- api/app.ts
+++ api/app.ts
@@ -1,4 +1,4 @@
 
-import app from "../app";  // Import your Express app from src/app.ts
+import app from "../src/app";  // Import your Express app from src/app.ts
 
 export default app;  // Export it for Vercel to handle as a serverless function
