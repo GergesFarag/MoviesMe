8d2496bddb360ab8fca9f040636d396a254e1e51
GergesFarag <georgefarag128@gmail.com>
2025-08-27 01:09:59 +0300

    finxing firebase

--- src/Config/firebase.ts
+++ src/Config/firebase.ts
@@ -1,27 +1,7 @@
-import admin from "firebase-admin";
-import fs from "fs";
-import path from "path";
-const base64ServiceAccountKey = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+import admin, { ServiceAccount } from "firebase-admin";
+import service_account from "../../firebase_service_account.json";
 
-if (!base64ServiceAccountKey) {
-  throw new Error(
-    "Firebase service account key is missing in environment variables"
-  );
-}
-
-// Decode the base64 string and write it to a temporary file
-const decodedKey = Buffer.from(base64ServiceAccountKey, "base64").toString(
-  "utf-8"
-);
-const keyFilePath = path.join(__dirname, "serviceAccountKey.json");
-
-fs.writeFileSync(keyFilePath, decodedKey);
-
-// Initialize Firebase Admin SDK
 admin.initializeApp({
-  credential: admin.credential.cert(require(keyFilePath)),
+  credential: admin.credential.cert(service_account as ServiceAccount),
 });
-
-// Clean up the temporary key file after initialization (optional)
-fs.unlinkSync(keyFilePath);
 export const firebaseAdmin = admin;
