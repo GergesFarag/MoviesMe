10649d66b029978a690ccc6c05cab2b2346b9cdc
GergesFarag <georgefarag128@gmail.com>
2025-08-28 23:35:11 +0300

    fix: file type assertion

--- src/Routes/user.routes.ts
+++ src/Routes/user.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from "express";
 import userController from "../Controllers/user.controller";
 import { authMiddle } from "../Middlewares/auth.middleware";
-import { upload } from "../Config/multer";
+import { imageUpload, upload } from "../Config/multer";
 const userRouter = Router();
 
 userRouter
   .route("/")
   .get(authMiddle, userController.getProfile)
-  .patch(authMiddle, upload.single("profilePicture"), userController.updateProfile);
+  .patch(authMiddle, imageUpload.single("profilePicture"), userController.updateProfile);
 userRouter.route("/lib").get(authMiddle, userController.getUserLibrary);
 userRouter.route("/fav").post(authMiddle, userController.toggleFav);
 userRouter.route("/lib/:itemId").delete(authMiddle, userController.deleteItem);
