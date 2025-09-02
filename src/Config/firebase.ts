b6f2d7798f1899eceaad0a7e62047a8e286fd4a2
GergesFarag <georgefarag128@gmail.com>
2025-09-03 01:05:49 +0300

    fix : Url and redis mem

--- src/Config/firebase.ts
+++ src/Config/firebase.ts
@@ -12,14 +12,7 @@ if (!admin.apps.length) {
       admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
       });
-    } else {
-      const service_account = require("../../ttov-a9677-firebase-adminsdk-fbsvc-6bd2137180.json");
-      admin.initializeApp({
-        credential: admin.credential.cert(service_account as ServiceAccount),
-      });
-      console.log("Firebase Admin initialized successfully with JSON file");
-    }
-    
+    } 
   } catch (error) {
     console.error("Error initializing Firebase Admin:", error);
     throw error;
