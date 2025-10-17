2d8347fa19907e190ef25ae4682b2d4e5a2b9c83
GergesFarag <georgefarag128@gmail.com>
2025-10-18 00:54:41 +0300

    update logic of cooldown

--- src/Controllers/otp.controller.ts
+++ src/Controllers/otp.controller.ts
@@ -3,14 +3,15 @@ import AppError from "../Utils/Errors/AppError";
 import catchError from "../Utils/Errors/catchError";
 export const requestOtp = catchError(async (req, res) => {
   const {phoneNumber} = req.body
+  if(!phoneNumber) throw new AppError('Phone number is required!')
   const otpService = OTPService.getInstance(phoneNumber);
   const response = await otpService.sendOTP();
   res.status(200).json(response);
 });
 export const verifyOtp = catchError(async (req, res) => {
   const { otp , phoneNumber } = req.body;
-  if(!otp){
-    throw new AppError("OTP is Required!");
+  if(!otp||!phoneNumber){
+    throw new AppError("OTP and Phone number are Required!");
   }
   const otpService = OTPService.getInstance(phoneNumber);
   const response = await otpService.verifyOTP(phoneNumber,otp);
